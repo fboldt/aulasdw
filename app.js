7443b2d9f9f662aac87c878840bca2857f87ab3b
Francisco Boldt <fboldt@gmail.com>
2022-09-05 10:54:59 -0300

    hard work static hello

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+const fs = require('fs');
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+    const url = req.url
+    console.log(url)
+    res.statusCode = 200;
+    if (url == "/hello.html") {
+        res.setHeader('Content-Type', 'text/html');
+        fs.readFile("hello.html", (err, data) => {
+            res.end(data)
+        })
+        return
+    }
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
