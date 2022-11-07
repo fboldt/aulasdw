d4f4cd456735a0ca1e55fbaf5f288c007f17a6d2
Francisco de Assis Boldt <fboldt@gmail.com>
2022-11-07 10:44:45 -0300

    db json

---
+++ db/persist.js
@@ -0,0 +1,22 @@
+const { readUsers, writeUsers } = require('./controller')
+
+const setUser = async (username, password) => {
+    const users = readUsers()
+    users.push({"username": username, "password": password})
+    return writeUsers(users)
+}
+
+const getPass = (username) => {
+    const users = readUsers()
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username == username) {
+            return users[i].password
+        }
+    }
+    return null
+}
+
+module.exports = {
+    setUser,
+    getPass,
+}
