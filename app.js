cbdae0a592142be2ef66c31a2dddeeaaff068304
Francisco de Assis Boldt <fboldt@gmail.com>
2023-03-27 18:59:40 -0300

    feat: use handlebars

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const express = require('express')
+import express from 'express'
+import { engine } from 'express-handlebars'
+
 const app = express()
 
-app.use(express.static('./public'))
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+// app.use(express.static('./public'))
+
+app.get("/", (req, res) => { res.render("home") })
+app.get("/sobre", (req, res) => { res.send("sobre") })
+app.use((req, res) => { res.send("404") })
+app.use((err, req, res, next) => { res.send("500") })
 
 const port = process.env.PORT || 3000
-const start = async () => {
-    try {
-        app.listen(port, () => {
-            console.log(`\n\nListen on port ${port}.`)
-        })
-    } catch (error) {
-        console.log(error)
-    }
-}
+app.listen(port, () => {
+    console.log(`\n\nListen on port ${port}.`)
+})
 
-start()
