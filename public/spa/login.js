cd83f64fbc53c85fd406d0786ca679e15586f8e3
Francisco de Assis Boldt <fboldt@gmail.com>
2023-05-26 09:06:09 -0300

    feat: display form login and link logout

--- public/spa/login.js
+++ public/spa/login.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    setFormLoginBehavior()
+    checkLogin()
 })
 
-function setFormLoginBehavior() {
+function checkLogin(){
+    username = localStorage.getItem("username")
+    if (username) {
+        displayLinkLogout(username)
+    } else {
+        displayFormLogin()
+    }
+}
+
+function displayFormLogin() {
     const loginSpace = document.querySelector('#login-space')
     loginSpace.innerHTML = `
     <form style="text-align: right;">
@@ -11,8 +20,40 @@ function setFormLoginBehavior() {
         <input type="submit" value="entrar" class="btn btn-primary" style="margin: 1px;">
     </form>`
     const formLogin = loginSpace.querySelector('form')
-    formLogin.addEventListener("submit", (ev) => {
+    formLogin.addEventListener("submit", function (ev) {
         ev.preventDefault()
-        console.log("login")
+        // console.log("login")
+        const payload = new URLSearchParams(new FormData(this))
+        sendLogin(payload)
+    })
+}
+
+function sendLogin(payload) {
+    fetch("/api/login", {
+        method: "POST",
+        body: payload,
+    })
+        .then(res => res.json())
+        .then(data => {
+            const { success, email, token } = data
+            if (success) {
+                localStorage.setItem("username", email)
+                localStorage.setItem("token", token)
+                displayLinkLogout(email)
+            }
+        })
+}
+
+function displayLinkLogout(email) {
+    const loginSpace = document.querySelector('#login-space')
+    loginSpace.innerHTML = `
+    <span id="username">${email}</span>
+    <a href="">logout</a>`
+    const linkLogout = loginSpace.querySelector("a")
+    linkLogout.addEventListener("click", function (ev) {
+        ev.preventDefault()
+        localStorage.removeItem("username")
+        localStorage.removeItem("token")
+        displayFormLogin()
     })
 }
