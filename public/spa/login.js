433d06e401cd89c9e08b69d2365898832cf958f8
Francisco de Assis Boldt <fboldt@gmail.com>
2023-06-02 08:26:04 -0300

    feat: include fail login message

--- public/spa/login.js
+++ public/spa/login.js
@@ -11,12 +11,13 @@ function checkLogin(){
     }
 }
 
-function displayFormLogin() {
+function displayFormLogin(msg="") {
     const loginSpace = document.querySelector('#login-space')
     loginSpace.innerHTML = `
     <form style="text-align: right;">
         <input type="email" name="email" id="email" placeholder="e-mail" style="margin: 1px;"><br>
         <input type="password" name="senha" id="senha" placeholder="senha" style="margin: 1px;"><br>
+        <span style="color: red;">${msg}</span>
         <input type="submit" value="entrar" class="btn btn-primary" style="margin: 1px;">
     </form>`
     const formLogin = loginSpace.querySelector('form')
@@ -40,7 +41,7 @@ function sendLogin(payload) {
                 localStorage.setItem("token", token)
                 displayLinkLogout(email)
             } else {
-                displayFormLogin()
+                displayFormLogin("Falha no login!")
             }
         })
 }
